0980841a323f912030e15c1cefab8c226c227bbd
Joshua Montross <joshua.montross@gmail.com>
2015-07-09 17:53:26 -0500

    updates to devise and controllers

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -240,6 +240,12 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
 
+  config.omniauth :stripe_connect,
+      ENV['STRIPE_CONNECT_CLIENT_ID'],
+      ENV['STRIPE_API_KEY'],
+      :scope => 'read_write',
+      :stripe_landing => 'register'
+
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.

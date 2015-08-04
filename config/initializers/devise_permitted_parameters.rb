1af36ed96b2fb46bf2c6486f580eeb26f50e2f33
Joshua Montross <joshua.montross@gmail.com>
2015-08-05 01:21:47 +0200

    ads donation rate as permitted param

--- config/initializers/devise_permitted_parameters.rb
+++ config/initializers/devise_permitted_parameters.rb
@@ -10,6 +10,7 @@ module DevisePermittedParameters
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :name
     devise_parameter_sanitizer.for(:account_update) << :name
+    devise_parameter_sanitizer.for(:account_update) << :donation_rate
   end
 
 end

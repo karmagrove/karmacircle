c7b1ff1a3bda77d1cf019f2fdd6eaa4008a38cd4
Joshua Montross <joshua.montross@gmail.com>
2015-08-06 21:40:45 +0200

    ads donor as user to donationchargers

--- db/migrate/20150801191655_add_donor_to_donation_charge.rb
+++ db/migrate/20150801191655_add_donor_to_donation_charge.rb
@@ -1,5 +1,5 @@
 class AddDonorToDonationCharge < ActiveRecord::Migration
   def change
-  	 add_reference :donation_charges, :donor, index: true, foreign_key: true
+  	 add_reference :donation_charges, :user, index: true, foreign_key: true
   end
 end

551621460f2d67fd2c6523dbb0d7149f0e4d1e3c
Joshua Montross <joshua.montross@gmail.com>
2015-08-06 22:19:24 +0200

    updates asset path for image assets

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -3,14 +3,14 @@ class Donation < ActiveRecord::Base
   belongs_to :donor
   has_many :donation_charges
 
-  def makePayments
+  def self.makePayments
   	User.all.each do |user|
   	  amount = {}
-  	  charges = DonationCharge.where(:status => "unpaid", :donor_id => user.id).each do |charge|
+  	  charges = DonationCharge.where(:status => "unpaid", :user_id => user.id).each do |charge|
   	  	amount[charge["charity_id"]] ||= amount[charge["charity_id"]] + charge.donation_amount
   	  end
 
-  	  customer = customer = Stripe::Customer.find(user.uid)
+  	  customer = Stripe::Customer.find(user.uid)
 
   	  if amount > 500 then
   	  	 charge = Stripe::Charge.create({

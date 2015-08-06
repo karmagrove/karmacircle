97e57e2da72c4fe2c268bdad7b43c981435b6eae
Joshua Montross <joshua.montross@gmail.com>
2015-08-06 22:35:23 +0200

    updates view to make all redirect to / on login

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -3,14 +3,17 @@ class ContentController < ApplicationController
 
   def charity
     redirect_to root_path, :notice => "Access denied." unless current_user.charity?
+
   end
 
   def patron
     redirect_to root_path, :notice => "Access denied." unless current_user.patron?
+    redirect_to "/", :notice => "You are logged in as a Patron"
   end
 
   def partner
     redirect_to root_path, :notice => "Access denied." unless current_user.partner?
+    redirect_to "/", :notice => "You are logged in as a Partner"
   end
 
 end

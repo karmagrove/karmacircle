77c037ede61c2001cbee640440044becf3c32211
Joshua Montross <joshua.montross@gmail.com>
2015-08-06 19:44:54 +0200

    updates donation to have method to pay charities

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -4,6 +4,25 @@ class Donation < ActiveRecord::Base
   has_many :donation_charges
 
   def makePayments
-  	DonationCharges.where(:status => "unpaid").sort_by(:user_id)
+  	User.all.each do |user|
+  	  amount = {}
+  	  charges = DonationCharge.where(:status => "unpaid", :donor_id => user.id).each do |charge|
+  	  	amount[charge["charity_id"]] ||= amount[charge["charity_id"]] + charge.donation_amount
+  	  end
+
+  	  customer = customer = Stripe::Customer.find(user.uid)
+
+  	  if amount > 500 then
+  	  	 charge = Stripe::Charge.create({
+           :amount => @amount, # amount in cents
+           :currency => "usd",
+           :customer => customer,
+           :description => description
+         },
+         {:stripe_account => charity.uid}
+         )        
+
+  	  end
+
   end
 end
--- app/models/donation_charge.rb
+++ app/models/donation_charge.rb
@@ -2,9 +2,10 @@ class DonationCharge < ActiveRecord::Base
   belongs_to :charity
   belongs_to :donation
   belongs_to :donor
-
-  after_initialize :set_default_status, :if => :new_record?
   enum status: [:unpaid, :denied, :paid]
+  
+  after_initialize :set_default_status, :if => :new_record?
+  
 
   def set_default_status
   	self.status || :unpaid

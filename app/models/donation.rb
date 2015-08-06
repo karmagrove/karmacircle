f8e782490c458409893e16b256c16d1397bde130
Joshua Montross <joshua.montross@gmail.com>
2015-08-06 18:54:18 +0200

    updates donation_charge and app with new model code for paying donatin_charges

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -2,4 +2,8 @@ class Donation < ActiveRecord::Base
   belongs_to :charity
   belongs_to :donor
   has_many :donation_charges
+
+  def makePayments
+  	DonationCharges.where(:status => "unpaid").sort_by(:user_id)
+  end
 end
--- app/models/donation_charge.rb
+++ app/models/donation_charge.rb
@@ -2,4 +2,11 @@ class DonationCharge < ActiveRecord::Base
   belongs_to :charity
   belongs_to :donation
   belongs_to :donor
+
+  after_initialize :set_default_status, :if => :new_record?
+  enum status: [:unpaid, :denied, :paid]
+
+  def set_default_status
+  	self.status || :unpaid
+  end
 end

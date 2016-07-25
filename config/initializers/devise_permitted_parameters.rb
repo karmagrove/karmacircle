ed477e09c9d8d5b8c425844e0dab766ea2de6924
Joshua Montross <joshua.montross@gmail.com>
2016-07-25 10:34:38 -0400

    updates initializer for devise to have the right params available

--- config/initializers/devise_permitted_parameters.rb
+++ config/initializers/devise_permitted_parameters.rb
@@ -13,6 +13,16 @@ module DevisePermittedParameters
     # devise_parameter_sanitizer.for(:account_update) << :donation_rate
     # devise_parameter_sanitizer.for(:account_update) << :business_name
 
+
+    #add some params
+    #devise_parameter_sanitizer.for(:accept_invitation).concat [:first_name, :last_name, :phone]
+    
+    # over ride params
+    devise_parameter_sanitizer.for(:accept_invitation) do |u|
+    u.permit(:name, :password, :business_name, :password_confirmation,
+             :invitation_token)
+    end
+
     devise_parameter_sanitizer.for(:account_update) {|u|
      u.permit(:name,:donation_rate,:business_name,:current_password,:public_profile,:community_profile)
     }

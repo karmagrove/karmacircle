447564b264141b2975144c35c16c1e7784471e7a
Joshua Montross <joshua.montross@gmail.com>
2016-08-03 20:16:40 -0400

    improves community view

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -18,7 +18,7 @@ CarrierWave.configure do |config|
     # endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
   }
   config.fog_directory  = 'karmagrove'                          # required
-  # config.fog_public     = false                                   # optional, defaults to true
+  config.fog_public     = true                                   # optional, defaults to true
   # config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {} # optional, defaults to {}
 
   if Rails.env.test? || Rails.env.cucumber?

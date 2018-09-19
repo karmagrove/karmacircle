68060f508432d56ad8610749e321a9cf3d791f91
Joshua Montross <joshua.montross@gmail.com>
2018-09-19 14:09:42 -0700

    updates with downcase for the email comparison

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -57,7 +57,7 @@ class Purchase < ActiveRecord::Base
   	 Rails.logger.info response
      user_id = JSON.parse(response)
   	 Rails.logger.info user_id
-     if (user_id['results'].length > 0) && (user_id['results'][0]['person']['email'] == self.buyer_email)
+     if (user_id['results'].length > 0) && (user_id['results'][0]['person']['email'].downcase == self.buyer_email.downcase)
   	   return user_id['results'][0]['person']['id'] 
      else
        return create_user
